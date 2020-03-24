e1120d3482cb9d5ee35add059994379c4b0ab2d1
jcarvargtz <52109543+jcarvargtz@users.noreply.github.com>
2020-03-23 22:10:50 -0600

    b gv

--- pantunfla/Model_jc.py
+++ pantunfla/Model_jc.py
@@ -40,80 +40,136 @@ from keras.applications import ResNet152V2, ResNet50
 
 # # # # # # # Create Model # # # # # #
 # Create inputs
+# def make_model(n_frames,dims,channels):
+#     input_1 = tf.keras.layers.Input(shape=[ n_frames, *dims, channels], name="input_1") 
+#     input_2 = tf.keras.layers.Input(shape=[ n_frames, *dims, channels], name="input_2")
+#     # input_3 = Input(shape=[ n_frames, *dims, channels], name="input_3")
+#     # Create first part of the model
+#     # x1 = TimeDistributed(ResNet50())(input_1)
+#     x1 = tf.keras.layers.TimeDistributed(tf.keras.layers.Conv2D(16,kernel_size=(7,7)),name="1.1")(input_1)
+#     x1 = tf.keras.layers.Activation("relu", name="act1.1")(x1)
+#     x1 = tf.keras.layers.TimeDistributed(tf.keras.layers.Conv2D(32,kernel_size=(5,5)),name="1.2")(x1)
+#     x1 = tf.keras.layers.Activation("relu", name="act1.2")(x1)
+#     x1 = tf.keras.layers.TimeDistributed(tf.keras.layers.BatchNormalization(),name="1.2b")(x1)
+#     x1 = tf.keras.layers.TimeDistributed(tf.keras.layers.Conv2D(64,kernel_size=(3,3)),name="1.3")(x1)
+#     x1 = tf.keras.layers.Activation("relu", name="act1.3")(x1)
+#     x1 = tf.keras.layers.TimeDistributed(tf.keras.layers.BatchNormalization(),name="1.3b")(x1)
+#     # x1 = tf.keras.layers.TimeDistributed(tf.keras.layers.MaxPool2D(),name="1.3m")(x1)
+#     x1 = tf.keras.layers.MaxPool3D()(x1)
+#     x1 = tf.keras.layers.TimeDistributed(tf.keras.layers.Flatten())(x1)
+#     # x1 = tf.keras.layers.LSTM(2,name="1.lstm")(x1)
+#     # x1 = tf.keras.layers.BatchNormalization(name="1.lstmb")(x1)
+#     # mod_1 = tf.keras.models.Model(inputs=input_1, outputs = x1)
+#     # Create second part of model
+#     # x2 = TimeDistributed(ResNet50())(input_2)
+#     x2 = tf.keras.layers.TimeDistributed(tf.keras.layers.Conv2D(16,kernel_size=(7,7)),name="2.1")(input_2)
+#     x2 = tf.keras.layers.Activation("relu", name="act2.1")(x2)
+#     x2 = tf.keras.layers.TimeDistributed(tf.keras.layers.Conv2D(32,kernel_size=(5,5)),name="2.2")(x2)
+#     x2 = tf.keras.layers.Activation("relu", name="act2.2")(x2)
+#     x2 = tf.keras.layers.TimeDistributed(tf.keras.layers.BatchNormalization(),name="2.2b")(x2)
+#     x2 = tf.keras.layers.TimeDistributed(tf.keras.layers.Conv2D(64,kernel_size=(3,3)),name="2.3")(x2)
+#     x2 = tf.keras.layers.Activation("relu", name="act2.3")(x2)
+#     x2 = tf.keras.layers.TimeDistributed(tf.keras.layers.BatchNormalization(),name="2.3b")(x2)
+#     # x2 = tf.keras.layers.TimeDistributed(tf.keras.layers.MaxPool2D(),name="2.3m")(x2)
+#     x2 = tf.keras.layers.MaxPool3D()(x2)
+#     x2 = tf.keras.layers.TimeDistributed(tf.keras.layers.Flatten())(x2)
+#     # mod_2 = tf.keras.models.Model(inputs=input_2, outputs = x2)
+#     # x4 = tf.keras.layers.TimeDistributed(tf.keras.layers.Concatenate([mod_1.output, mod_2.output]))
+#     x4 = tf.keras.layers.concatenate(inputs=[x1,x2],axis =-1)
+#     x4 = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(24,activation = "relu"))(x4)
+#     x4 = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(24,activation = "relu"))(x4)
+#     # x4 = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(24,activation = "relu"))(x4)
+#     # x4 = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(24,activation = "relu"))(x4)
+#     # x4 = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(24,activation = "relu"))(x4)
+#     x4 = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(1,activation = "sigmoid"))(x4)
+#     x4 = tf.keras.layers.MaxPool1D()(x4)
+#     x4 = tf.keras.layers.LSTM(1,name="2.lstm")(x4)
+#     x4 = tf.keras.layers.Dense(1,activation="sigmoid")(x4)
+#     # x2 = tf.keras.layers.LSTM(2,name="2.lstm")(x2)
+#     # x2 = tf.keras.layers.BatchNormalization(name="2.lstmb")(x2)
+#     # mod_2 = tf.keras.models.Model(inputs=input_2, outputs = x2)
+#     # Create third part of model
+#     # x3 = TimeDistributed(ResNet50())(input_3)
+#     # x3 = TimeDistributed(Conv2D(32,kernel_size=(7,7)),name="3.1")(input_3)
+#     # x3 = Activation("relu", name="act3.1")(x3)
+#     # x3 = TimeDistributed(Conv2D(64,kernel_size=(5,5)),name="3.2")(x3)
+#     # x3 = Activation("relu", name="act3.2")(x3)
+#     # x3 = TimeDistributed(BatchNormalization(),name="3.2b")(x3)
+#     # x3 = TimeDistributed(Conv2D(128,kernel_size=(3,3)),name="3.3")(x3)
+#     # x3 = Activation("relu", name="act3.3")(x3)
+#     # x3 = TimeDistributed(BatchNormalization(),name="3.3b")(x3)
+#     # x3 = TimeDistributed(MaxPool2D(),name="3.3m")(x3)
+#     # x3 = TimeDistributed(Flatten())(x3)
+#     # x3 = LSTM(2,name="3.lstm")(x3)
+#     # x3 = BatchNormalization(name="3.lstmb")(x3)
+#     # mod_3 = Model(inputs=input_3, outputs = x3)
+#     # Join parts 1,2,3 of model
+#     # x4 =  tf.keras.layers.concatenate([mod_1.output, mod_2.output])#, mod_3.output
+#     # x4 = tf.keras.layers.Dense(24, activation ="relu")(x4)
+#     # x4 = tf.keras.layers.Dense(24, activation ="relu")(x4)
+#     # x4 = tf.keras.layers.Dense(24, activation ="relu")(x4)
+#     # x4 = tf.keras.layers.Dense(24, activation ="relu")(x4)
+#     # x4 = Dense(24, activation ="relu")(x4)
+#     # x4 = Dense(24, activation ="relu")(x4)
+#     # x4 = tf.keras.layers.Dense(1,  activation ="sigmoid")(x4)
+#     # mod_4 = tf.keras.models.Model(inputs=[mod_1.input,mod_2.input], outputs=x4)#,mod_3.input
+#     mod_4 = tf.keras.models.Model(inputs=[input_1,input_2], outputs=x4)
+#     return mod_4
+
+import tensorflow as tf
 def make_model(n_frames,dims,channels):
     input_1 = tf.keras.layers.Input(shape=[ n_frames, *dims, channels], name="input_1") 
     input_2 = tf.keras.layers.Input(shape=[ n_frames, *dims, channels], name="input_2")
-    # input_3 = Input(shape=[ n_frames, *dims, channels], name="input_3")
-    # Create first part of the model
-    # x1 = TimeDistributed(ResNet50())(input_1)
-    x1 = tf.keras.layers.TimeDistributed(tf.keras.layers.Conv2D(16,kernel_size=(7,7)),name="1.1")(input_1)
-    x1 = tf.keras.layers.Activation("relu", name="act1.1")(x1)
-    x1 = tf.keras.layers.TimeDistributed(tf.keras.layers.Conv2D(32,kernel_size=(5,5)),name="1.2")(x1)
-    x1 = tf.keras.layers.Activation("relu", name="act1.2")(x1)
+
+    x1 = tf.keras.layers.ConvLSTM2D(32,kernel_size=(7,7),name="1.1", return_sequences=True)(input_1)
+    x1 = tf.keras.layers.ConvLSTM2D(64,kernel_size=(5,5),name="1.2")(x1)
     x1 = tf.keras.layers.TimeDistributed(tf.keras.layers.BatchNormalization(),name="1.2b")(x1)
-    x1 = tf.keras.layers.TimeDistributed(tf.keras.layers.Conv2D(64,kernel_size=(3,3)),name="1.3")(x1)
-    x1 = tf.keras.layers.Activation("relu", name="act1.3")(x1)
+    x1 = tf.keras.layers.Conv2D(128,kernel_size=(3,3),name="1.3")(x1)
     x1 = tf.keras.layers.TimeDistributed(tf.keras.layers.BatchNormalization(),name="1.3b")(x1)
-    x1 = tf.keras.layers.TimeDistributed(tf.keras.layers.MaxPool2D(),name="1.3m")(x1)
-    x1 = tf.keras.layers.TimeDistributed(tf.keras.layers.Flatten())(x1)
-    # x1 = tf.keras.layers.LSTM(2,name="1.lstm")(x1)
-    # x1 = tf.keras.layers.BatchNormalization(name="1.lstmb")(x1)
-    # mod_1 = tf.keras.models.Model(inputs=input_1, outputs = x1)
-    # Create second part of model
-    # x2 = TimeDistributed(ResNet50())(input_2)
-    x2 = tf.keras.layers.TimeDistributed(tf.keras.layers.Conv2D(16,kernel_size=(7,7)),name="2.1")(input_2)
-    x2 = tf.keras.layers.Activation("relu", name="act2.1")(x2)
-    x2 = tf.keras.layers.TimeDistributed(tf.keras.layers.Conv2D(32,kernel_size=(5,5)),name="2.2")(x2)
-    x2 = tf.keras.layers.Activation("relu", name="act2.2")(x2)
+    x1 = tf.keras.layers.MaxPool2D()(x1)
+    # x1 = tf.keras.layers.TimeDistributed(tf.keras.layers.Flatten())(x1)
+
+    x2 = tf.keras.layers.ConvLSTM2D(32,kernel_size=(7,7),name="2.1",return_sequences=True)(input_2)
+    x2 = tf.keras.layers.ConvLSTM2D(64,kernel_size=(5,5),name="2.2")(x2)
     x2 = tf.keras.layers.TimeDistributed(tf.keras.layers.BatchNormalization(),name="2.2b")(x2)
-    x2 = tf.keras.layers.TimeDistributed(tf.keras.layers.Conv2D(64,kernel_size=(3,3)),name="2.3")(x2)
-    x2 = tf.keras.layers.Activation("relu", name="act2.3")(x2)
+    x2 = tf.keras.layers.Conv2D(128,kernel_size=(3,3),name="2.3")(x2)
     x2 = tf.keras.layers.TimeDistributed(tf.keras.layers.BatchNormalization(),name="2.3b")(x2)
-    x2 = tf.keras.layers.TimeDistributed(tf.keras.layers.MaxPool2D(),name="2.3m")(x2)
-    x2 = tf.keras.layers.TimeDistributed(tf.keras.layers.Flatten())(x2)
-    # mod_2 = tf.keras.models.Model(inputs=input_2, outputs = x2)
-    # x4 = tf.keras.layers.TimeDistributed(tf.keras.layers.Concatenate([mod_1.output, mod_2.output]))
-    x4 = tf.keras.layers.concatenate(inputs=[x1,x2],axis =-2)
+    x2 = tf.keras.layers.MaxPool2D()(x2)
+    # x2 = tf.keras.layers.TimeDistributed(tf.keras.layers.Flatten())(x2)
+
+    x4 = tf.keras.layers.concatenate(inputs=[x1,x2],axis =-1)
     x4 = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(24,activation = "relu"))(x4)
     x4 = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(24,activation = "relu"))(x4)
-    # x4 = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(24,activation = "relu"))(x4)
-    # x4 = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(24,activation = "relu"))(x4)
-    # x4 = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(24,activation = "relu"))(x4)
-    x4 = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(1,activation = "sigmoid"))(x4)
-    x4 = tf.keras.layers.LSTM(1,name="2.lstm")(x4)
+    x4 = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(24,activation = "relu"))(x4)
+    x4 = tf.keras.layers.MaxPool2D()(x4)
+    # x4 = tf.keras.layers.LSTM(1,name="lstm")(x4)
+    x4 = tf.keras.layers.Flatten()(x4)
     x4 = tf.keras.layers.Dense(1,activation="sigmoid")(x4)
-    # x2 = tf.keras.layers.LSTM(2,name="2.lstm")(x2)
-    # x2 = tf.keras.layers.BatchNormalization(name="2.lstmb")(x2)
-    # mod_2 = tf.keras.models.Model(inputs=input_2, outputs = x2)
-    # Create third part of model
-    # x3 = TimeDistributed(ResNet50())(input_3)
-    # x3 = TimeDistributed(Conv2D(32,kernel_size=(7,7)),name="3.1")(input_3)
-    # x3 = Activation("relu", name="act3.1")(x3)
-    # x3 = TimeDistributed(Conv2D(64,kernel_size=(5,5)),name="3.2")(x3)
-    # x3 = Activation("relu", name="act3.2")(x3)
-    # x3 = TimeDistributed(BatchNormalization(),name="3.2b")(x3)
-    # x3 = TimeDistributed(Conv2D(128,kernel_size=(3,3)),name="3.3")(x3)
-    # x3 = Activation("relu", name="act3.3")(x3)
-    # x3 = TimeDistributed(BatchNormalization(),name="3.3b")(x3)
-    # x3 = TimeDistributed(MaxPool2D(),name="3.3m")(x3)
-    # x3 = TimeDistributed(Flatten())(x3)
-    # x3 = LSTM(2,name="3.lstm")(x3)
-    # x3 = BatchNormalization(name="3.lstmb")(x3)
-    # mod_3 = Model(inputs=input_3, outputs = x3)
-    # Join parts 1,2,3 of model
-    # x4 =  tf.keras.layers.concatenate([mod_1.output, mod_2.output])#, mod_3.output
-    # x4 = tf.keras.layers.Dense(24, activation ="relu")(x4)
-    # x4 = tf.keras.layers.Dense(24, activation ="relu")(x4)
-    # x4 = tf.keras.layers.Dense(24, activation ="relu")(x4)
-    # x4 = tf.keras.layers.Dense(24, activation ="relu")(x4)
-    # x4 = Dense(24, activation ="relu")(x4)
-    # x4 = Dense(24, activation ="relu")(x4)
-    # x4 = tf.keras.layers.Dense(1,  activation ="sigmoid")(x4)
-    # mod_4 = tf.keras.models.Model(inputs=[mod_1.input,mod_2.input], outputs=x4)#,mod_3.input
+
     mod_4 = tf.keras.models.Model(inputs=[input_1,input_2], outputs=x4)
     return mod_4
 
-make_model(15,(224,224),3).summary()
+
+
+
+
+make_model(30,(224,224),3).summary()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # # Create callbacks, metrics, loss, and Generator # #
 # Callbacks
 # saved_model_path = "weights-improvement-{epoch:02d}-{val_accuracy:.2f}.hdf5"
