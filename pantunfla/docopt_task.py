0c545637a5aaf02e13ad9c30a4796e5c76898e87
jcarvargtz <52109543+jcarvargtz@users.noreply.github.com>
2020-03-24 20:26:53 -0600

    m jhnbgvc

--- pantunfla/docopt_task.py
+++ pantunfla/docopt_task.py
@@ -134,6 +134,6 @@ if __name__ == '__main__':
 
     model.summary()
     print("11")
-    model.fit_generator(generator = gener,callbacks=callbacks_list,validation_data=val,use_multiprocessing=False,workers=0,verbose=2,epochs=500,pickle_safe=False)#,max_queue_size=5,pickle_safe=False)
+    model.fit_generator(generator = gener,callbacks=callbacks_list,validation_data=val,use_multiprocessing=False,workers=0,verbose=2,epochs=500,max_queue_size=5)
 
     # Make_predicctions
